f22a32bd216014c2f17fc6e65b61edee26d4ad17
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2024-11-10 01:09:53 -0600

    modified:   bin/Semaforo.class 	modified:   src/Semaforo.java

--- src/Semaforo.java
+++ src/Semaforo.java
@@ -1,6 +1,6 @@
 public class Semaforo {
     private static int semaforo = 1;  // Semáforo inicializado a 1
-    private static final Object lock = new Object();  // Objeto de bloqueo
+    private static final Object lock = new Object();  // Objeto de bloqueo para realizar la exclusion mutua
 
     // Función que simula modificar un archivo
     public static void modificarArchivo(int id) {
@@ -9,16 +9,17 @@ public class Semaforo {
         // Simulamos la modificación del archivo
         System.out.println("Hilo " + id + " está modificando el archivo.");
         try {
-            Thread.sleep(1000);  // Simula el tiempo de modificación
+            Thread.sleep(3000);  // Simula el tiempo de modificación
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
         liberarSemaforo();  // Liberamos el semáforo
     }
+
+
     // Función para esperar a que el semáforo esté disponible
     public static void esperarSemaforo() {
-        synchronized (lock) {
+        synchronized (lock) {//codigo que solo un hilo puede ejecutar a la vez (synchronized)
             while (semaforo <= 0) {
                 try {
                     lock.wait();  // Espera activa si el semáforo no está disponible
@@ -32,7 +33,7 @@ public class Semaforo {
 
     // Función para liberar el semáforo
     public static void liberarSemaforo() {
-        synchronized (lock) {
+        synchronized (lock) {// codigo que solo un hilo puede ejecutar a la vez (synchronized)
             semaforo++;  // Liberamos el semáforo
             lock.notify();  // Notificamos a otros hilos que el semáforo está disponible
         }
